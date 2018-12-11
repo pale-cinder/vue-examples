b538f8c38a284a2a583b4aaee7b6d4ecdf10d075
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-11 15:40:21 -0800

    footer

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -7,12 +7,14 @@
       <img src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Circle_%28transparent%29.png" width="112" height="28">
     </a>
 
+
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
+
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
@@ -63,18 +65,70 @@
   </div>
 </nav>
   <router-view></router-view>
+
+  <footer class="footer is-primary">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <p>That’s why we collectively buy shit we don’t need, go to bed with people we don’t love, and try to work hard to get approval of people we don’t like.</p>
+        </div>
+        <div class="column has-text-right">
+          <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+          <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data:function(){
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive;
+    }
+  }
 }
 </script>
 
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass'
 @import 'mq'
+
+.navbar
+  background-color: #393939
+  a:hover
+    color: #00d1b2
+
+.navber-left a
+  color: #fff
+  font-weight: bold
+
+a.r-item
+  color: #00d1b2
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: #00d1b2
+    &:hover
+      background-color: #F1F1F1
+
+.navber-link a
+  color: #00d1b2
+
+.footer
+  background-color: #00d1b2 
+  color: #fff
+
+  .icon
+    color: #fff
+    marging-left: 20px
 
 
 </style>
