dad4568af98b7785c9f2eed4b4327d62e329e046
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-10 16:02:48 -0800

    router links

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,8 +1,31 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">MyCompany</a>
+        </div>
+
+        <span class="nav-toggle>
+          <span></span>
+          <span></span>
+          <span>
+        </span>
+
+        <div class="nav-right nav-menu">
+
+        <router-link to="/" class="nav-item r-item">Home</router-link>
+        <router-link to="/" class="nav-item r-item">About</router-link>
+        <router-link to="/" class="nav-item r-item">Features</router-link>
+        <router-link to="/" class="nav-item r-item">FAQ</router-link>
+
+
+        </div>
+      </div>
+    </div>
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
@@ -13,6 +36,7 @@ export default {
 
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass'
+@import 'mq'
 
 #app 
   font-family: 'Avenir', Helvetica, Arial, sans-serif
